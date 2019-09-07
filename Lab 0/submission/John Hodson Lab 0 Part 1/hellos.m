74c9619daf75ae293d08b131d83a48b05c91df40
John Hodson <hodsonus@gmail.com>
2019-09-07 15:00:20 -0400

    removed uncompressed Lab 0 submission

--- Lab 0/submission/John Hodson Lab 0 Part 1/hellos.m
+++
@@ -1,8 +0,0 @@
-function hellos(n)
-    if (~isscalar(n) || n < 1)
-        error('Input must be a positive scalar');
-    end
-    for i=1:n
-        fprintf("hello\n");
-    end
-end
--- Lab 0/submission/John Hodson Lab 0 Part 1/oddsummer.m
+++
@@ -1,6 +0,0 @@
-function res = oddsummer(n)
-    if ~isscalar(n) || n < 1
-        error('Input must be a positive scalar');
-    end
-    res = sum(1:2:n);
-end
